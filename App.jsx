4c65a795a278098c23b06e3eb65cef1f8646ecd7
prajwaltripathi12 <128091316+prajwaltripathi12@users.noreply.github.com>
2024-08-30 13:52:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Weather from './components/Weather'
+const App = () => {
+        return ( < div className = 'App' > < Weather / > < /div>)
+        }
+
+        export default App
